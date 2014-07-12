e5fc73bf3ab32969539929047f568d044c89040f
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-07-12 23:30:08 +0100

    Completed chapter 1 up to, but not including, "Creating Functions: fn."

--- ch-01/src/ch_01/core.clj
+++ ch-01/src/ch_01/core.clj
@@ -163,3 +163,173 @@ pizza
 (def v :v)
 (false? (var? v))
 (true? (var? #'v))
+
+(quote x)
+'x
+'(+ x x)
+(list '+ 'x 'x)
+
+;; Reader trick quote form to be read
+''x
+'@x
+'#(+ % %)
+'`(a b ~c) ; differs from lein repl. Don't know why.
+
+;; REPL prints "hi" before printing 120.
+(do
+  (println "hi")
+  (apply * [4 5 6]))
+
+;; Again, the REPL prints the formatted text.
+(let [a (inc (rand-int 6))
+      b (inc (rand-int 6))]
+  (println (format "You rolled a %s and a %s." a b))
+  (+ a b))
+
+(def p "foo")
+p
+(var p)
+
+(defn hypot
+  [x y]
+  (let [x2 (* x x)
+        y2 (* y y)]
+    (Math/sqrt (+ x2 y2))))
+(= 5.0 (hypot 3 4))
+
+;; Sequential destructuring.
+(def v [42 "foo" 99.2 [5 12]])
+
+;; Various ways to access list items.
+(first v)
+(second v)
+(last v)
+(nth v 2)
+(nth v 0)
+(nth v -1)
+(v 2)
+(.get v 2)
+
+(+ (first v) (v 2))
+(+ (first v) (first (last v)))
+
+;; Sequential Destructuring.
+(= (let [[x y z] v]
+    (+ x z))
+   141.2)
+
+(= (let [[x _ _ [y z]] v]
+    (+ x y z))
+   59)
+
+(rest v) ;; a sequence.
+(= (let [[x & rest] v]
+     rest)
+   (rest v))
+
+
+(= (let [[x _ z :as original-vector] v]
+     (conj original-vector (+ x z)))
+   [42 "foo" 99.2 [5 12] 141.2])
+
+;; Map destructuring.
+
+(def m {:a 5
+        :b 6
+        :c [7 8 9]
+        :d {:e 10 :f 11}
+        "foo" 88
+        42 false})
+
+(= (let [{a :a
+         b :b} m]
+    (+ a b))
+   11)
+
+(= 100
+   (let [{f "foo"} m]
+     (+ f 12)))
+(false? (let [{v 42} m] v))
+
+;; One can apply map "destructuring" to vectors.
+(= -17
+   (let [{x 3 y 8} [12 0 0 -18 44 6 0 0 1]]
+     (+ x y)))
+
+(= 20
+   (let [{{e :e} :d} m]
+     (* 2 e)))
+
+;; Combined map and sequence destructuring.
+(= 16
+   (let [{[x _ y] :c} m]
+     (+ x y)))
+
+(def map-in-vector ["James" {:birthday (java.util.Date. 73 1 6)}])
+(not (false? (re-matches #"James was born on Tue Feb 06 00:00:00 [A-Z]{3} 1973"
+       (let [[name {bd :birthday}] map-in-vector]
+         (str name " was born on " bd)))))
+
+(let [{r1 :x r2 :y :as randoms}
+      (zipmap [:x :y :z] (repeatedly (partial rand-int 10)))
+      result (assoc randoms :sum (+ r1 r2))]
+  (or (= 4 (count result))
+      (contains? :sum)
+      (= (+ r1 r2) (:sum result))))
+
+
+;; Default map destructuring values.
+(= 55
+   (let [{k :unknown
+          x :a
+          :or {k 50}} m]
+     (+ k x)))
+
+;; Binding values to keys names
+;; We often bind values to names of keys. For example,
+;; name is bound to result of applying :name and
+;; age is bound to result of applying :age.
+(def chas {:name "Chas" :age 31 :location "Massachusetts"})
+(= "Chas is 31 years old and lives in Massachusetts."
+   (let [{name :name
+          age :age
+          location :location} chas]
+     (str name " is " age " years old and lives in " location ".")))
+
+;; Since our map uses keywords for keys, we use the
+;; :keys keyword to specify the keys whose values we map.
+(= "Chas is 31 years old and lives in Massachusetts."
+   (let [{:keys [name age location]} chas]
+     (str name " is " age " years old and lives in " location ".")))
+
+;; If the keys are string values, we use :strs.
+(def brian {"name" "Brian"
+            "age" 31
+            "location" "British Columbia"})
+(= "Brian is 31 years old and lives in British Columbia."
+   (let [{:strs [name age location]} brian]
+     (str name " is " age " years old and lives in " location ".")))
+
+;; Finally, if the keys are symbols, we use :syms.
+(def christophe {'name "Christophe"
+                 'age 33
+                 'location "Rhone-Alpes"})
+(= "Christophe is 33 years old and lives in Rhone-Alpes."
+   (let [{:syms [name age location]} christophe]
+     (format "%s is %s years old and lives in %s." name age location)))
+
+
+;; Destructuring rest sequences as map key/value pairs.
+(def user-info ["robert8990" 2011 :name "Bob" :city "Boston"])
+(= "Bob is in Boston."
+   (let [[username account & extra-info] user-info
+         {:keys [name city]} extra-info]
+     (format "%s is in %s." name city)))
+
+;; Even simpler.
+(= "Bob is in Boston."
+   (let [[username account & {:keys [name city]}] user-info]
+     (format "%s is in %s." name city)))
+
+
+
